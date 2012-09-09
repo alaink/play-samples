25d998c0e273c281f22fdf4da9277eca7f8a1084
alain kajangwe <alain.kajangwe@gmail.com>
2012-09-09 18:50:50 +0200

    Posts

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,13 +5,18 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+	@Before
+	public void setup() {
+		Fixtures.deleteDatabase();
+	}
+	
     @Test
     public void createAndRetrieveUser() {
     	// create a new user and save it
     	new User("bob@gmail.com", "secret", "Bob").save();
     	
     	// retrieve the user with email address bob@gmail.com
-    	User bob = User.find("byEmailAndPassword", "bob.gmail.com", "secret").first();
+    	User bob = User.find("byEmail", "bob@gmail.com").first();
     	System.out.println("user: " + bob);
     	
     	// Test
@@ -28,6 +33,30 @@ public class BasicTest extends UnitTest {
     	assertNotNull(User.connect("bob@gmail.com", "secret"));
     	assertNull(User.connect("bob@gmail.com", "badpassword"));
     	assertNull(User.connect("tom@gmail.com", "secret"));
+    }
+    
+    @Test
+    public void createPost() {
+    	// create a new user and save it
+    	User bob = new User("bob@gmail.com", "secret", "Bob").save();
+    	// create new post
+    	new Post(bob, "My First Post", "Hello World!").save();
+    	
+    	// Test that the post has been created
+    	assertEquals(1, Post.count());
+    	
+    	// Retrieve all posts created by Bob
+    	List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
+    	System.out.println("posts: " + bobPosts.get(0).content);
+    	
+    	// Tests
+    	assertEquals(1, bobPosts.size());
+    	Post firstPost = bobPosts.get(0);
+    	assertNotNull(firstPost);
+    	assertEquals(bob, firstPost.author);
+    	assertEquals("My First Post", firstPost.title);
+    	assertEquals("Hello World!", firstPost.content);
+    	assertNotNull(firstPost.postedAt);
     }
 
 }
